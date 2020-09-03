20e060927a96de535b1815d954d74fcd48404197
Cristi Cismas <cristicismas99@gmail.com>
2020-09-03 22:55:28 +0300

    Prettify cycles.js

--- utils/cycles.js
+++ utils/cycles.js
@@ -6,20 +6,19 @@ export const getWorldStates = async () => {
   return worldStates;
 };
 
-const getCambionTime = (cycle) => {
+const getCambionTime = cycle => {
   const currentTime = new Date();
   const timeDifference = new Date(new Date(cycle.expiry) - currentTime);
   return `${timeDifference.getUTCHours()}h ${timeDifference.getUTCMinutes()}m ${timeDifference.getUTCSeconds()}s`;
-}
+};
 
 const formatWorldStates = async () => {
   const worldCycles = await fetchWorldCyclesData();
 
-
   return worldCycles.map((cycle, index) => {
     const worldName = Object.keys(WORLDS)[index];
 
-    let timeLeft = worldName === 'cambion' ? getCambionTime(cycle)  : cycle.timeLeft;
+    let timeLeft = worldName === 'cambion' ? getCambionTime(cycle) : cycle.timeLeft;
 
     const currentState = worldName === 'cambion' ? cycle.active : cycle.state;
 
@@ -27,9 +26,9 @@ const formatWorldStates = async () => {
       name: worldName,
       state: currentState,
       timeLeft
-    }
+    };
   });
-}
+};
 
 const fetchWorldCyclesData = async () => {
   return Promise.all([
